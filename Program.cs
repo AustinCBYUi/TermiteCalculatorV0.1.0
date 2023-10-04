9ca9e49b7a5708cafe8f38d91ece331af87033cf
AustinCBYUi <131830841+AustinCBYUi@users.noreply.github.com>
2023-10-03 23:19:55 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TermiteCalculatorV0._1._0;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Existing Customer? Yes or no.?");
+string input =  Console.ReadLine().ToLower();
+
+
+switch(input)
+{
+    case "yes":
+        Console.WriteLine("CustomerName: ");
+        string cusName = Console.ReadLine();
+        Console.WriteLine("PhoneNum: ");
+        string phone = Console.ReadLine();
+        Console.WriteLine("JobCode: ");
+        string job = Console.ReadLine();
+
+        NewInquiry newCustomer = new NewInquiry(cusName, phone, job);
+        break;
+
+    case "no":
+        Console.WriteLine();
+        break;
+}
